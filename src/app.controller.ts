3afb3aefd29a9eeef4902adebb82f62811c827de
Jan-Niclas Struewer <jan-niclas.struewer@iem.fraunhofer.de>
2020-06-08 09:05:27 +0200

    started generating swagger from code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { RegisterForGameDto } from './models/RegisterForGame';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/register')
+  register(@Body() registerForGame: RegisterForGameDto): string {
+    return 'registered';
   }
 }
