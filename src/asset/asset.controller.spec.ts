340fc9521dd32ebc391d4d0beb4e08303b4d3e63
Jan-Niclas Struewer <jan-niclas.struewer@iem.fraunhofer.de>
2020-06-29 08:39:40 +0200

    fixed asset controller and service test setup

--- src/asset/asset.controller.spec.ts
+++ src/asset/asset.controller.spec.ts
@@ -1,13 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AssetController } from './asset.controller';
+import { AssetService } from './asset.service';
 
 describe('Asset Controller', () => {
   let controller: AssetController;
-
+  let assetService = { createAsset: () => {}, updateAsset: () => {}};
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AssetController],
-    }).compile();
+      providers: [AssetService]
+    }).overrideProvider(AssetService).useValue(assetService).compile();
 
     controller = module.get<AssetController>(AssetController);
   });
--- src/asset/asset.service.spec.ts
+++ src/asset/asset.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AssetService } from './asset.service';
+import { SocketModule } from '../socket/socket.module';
+import { DbConnectorModule } from '../db-connector/db-connector.module';
 
 describe('AssetService', () => {
   let service: AssetService;
@@ -7,6 +9,7 @@ describe('AssetService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [AssetService],
+      imports: [SocketModule, DbConnectorModule]
     }).compile();
 
     service = module.get<AssetService>(AssetService);
