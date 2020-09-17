e34524ef477e1cf2fc0de1eb908fd5f98fea8a72
Jan-Niclas Struewer <jan-niclas.struewer@iem.fraunhofer.de>
2020-09-17 19:28:07 +0200

    using environment variable to detect the used port

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   fs.writeFileSync('./openApi.json', JSON.stringify(document));
 
   SwaggerModule.setup('api', app, document);
-
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
